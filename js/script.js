54dd24a15d3041c6f99c551de5eb4b17a8d778a9
iddimsangi <adammsangi@gmail.com>
2022-04-14 09:44:39 +0300

    improve: add roll dice functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+const btnRoll = document.querySelector('.btn--roll');
+const diceImg = document.querySelector('.dice');
+const currentScorePl1 = document.querySelector('#current--0');
+const scorePl1 = document.querySelector('#score--0');
+let currentScore = 0;
+diceImg.classList.add('hidden');
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceImg.classList.remove('hidden');
+  diceImg.src = `../img/dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore = currentScore + dice;
+    currentScorePl1.textContent = currentScore;
+  } else {
+  }
+});
