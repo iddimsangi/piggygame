4712530cce40070eb7f43f9b9da6ec1581221a11
iddimsangi <adammsangi@gmail.com>
2022-04-21 13:03:28 +0300

    improve: add hold button functionality

--- js/script.js
+++ js/script.js
@@ -2,17 +2,30 @@
 const playerO1 = document.querySelector('.player--0');
 const playerO2 = document.querySelector('.player--1');
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 const diceImg = document.querySelector('.dice');
 const player01Score = document.querySelector('#score--0');
 const player02Score = document.querySelector('#score--1');
 const currentScorePl1 = document.querySelector('#current--0');
 const scorePl1 = document.querySelector('#score--0');
+
 const scores = [0, 0];
 let playerActive = 0;
 let currentScore = 0;
 diceImg.classList.add('hidden');
 player01Score.textContent = 0;
 player02Score.textContent = 0;
+
+const switchPlayerHandler = () => {
+  document.querySelector(`#current--${playerActive}`).textContent = 0;
+  currentScore = 0;
+  playerActive = playerActive === 0 ? 1 : 0;
+  playerO1.classList.toggle('player--active');
+  playerO2.classList.toggle('player--active');
+};
+
+// const holdScoreHandler =;
+
 btnRoll.addEventListener('click', () => {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceImg.classList.remove('hidden');
@@ -22,10 +35,13 @@ btnRoll.addEventListener('click', () => {
     document.querySelector(`#current--${playerActive}`).textContent =
       currentScore;
   } else {
-    document.querySelector(`#current--${playerActive}`).textContent = 0;
-    currentScore = 0;
-    playerActive = playerActive === 0 ? 1 : 0;
-    playerO1.classList.toggle('player--active');
-    playerO2.classList.toggle('player--active');
+    switchPlayerHandler();
   }
+});
+btnHold.addEventListener('click', () => {
+  scores[playerActive] += currentScore;
+  document.querySelector(`#score--${playerActive}`).textContent =
+    scores[playerActive];
+  diceImg.classList.add('hidden');
+  switchPlayerHandler();
 });
